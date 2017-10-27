96ba42053e5aa759fff6b14bfcea4da894be48c3
Sibin Francis <sibinfrancisaj@gmail.com>
2017-10-27 16:08:00 +0530

    booking

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -22,6 +22,7 @@ use  App\TravelType;
 use  App\TripActivity;
 use Krlove\EloquentModelGenerator\Model\EloquentModel;
 use App\Booking;
+use PhpParser\Node\Expr\Cast\Bool_;
 
 class HomepageController extends Controller
 {
@@ -37,18 +38,16 @@ class HomepageController extends Controller
     {
         if (!empty($id)) {
 
-            if($id=='All')
-            {
+            if ($id == 'All') {
                 $packages = Package::with(['travelType', 'tripItineraries', 'tripItineraries.tripActivities', 'placesToVisits', 'tags', 'tripSubCategory', 'tripCategory'])
                     ->get();
                 if (!empty($packages)) {
-                    return view('homepage.category_detailed', ['packages' => $packages,'header'=>"Explore amazing packages "]);
+                    return view('homepage.category_detailed', ['packages' => $packages, 'header' => "Explore amazing packages "]);
                 } else {
                     return view('homepage.category_detailed', ['packages' => null]);
                 }
 
-            }else
-            {
+            } else {
                 $category = TripCategory::where('id', $id)->first();
                 if (!empty($category)) {
 
@@ -56,7 +55,7 @@ class HomepageController extends Controller
                         ->where('trip_category_id', $id)
                         ->get();
                     if (!empty($packages)) {
-                        return view('homepage.category_detailed', ['packages' => $packages,'header'=>"Popular packages for ".$category->name]);
+                        return view('homepage.category_detailed', ['packages' => $packages, 'header' => "Popular packages for " . $category->name]);
                     } else {
                         return view('homepage.category_detailed', ['packages' => null]);
                     }
@@ -67,94 +66,95 @@ class HomepageController extends Controller
             }
 
 
-        }else {
+        } else {
             return redirect('homepage');
         }
     }
 
-public function bookingRequest($id=null)
-{
-    $packages = Package::where('id', $id) ->first();
-
-    if(!empty($packages))
+    public function bookingRequest($id = null)
     {
-        return view('homepage.booking', ['packages' => $packages]);
-    }
-    else
-    {
-        return redirect('homepage');
-    }
-}
+        $packages = Package::where('id', $id)->first();
 
-public function packageDetailed($id=null)
-{
-
-    if(!empty($id))
-    {
-        $package = Package::where('id',$id)
-            ->with(['travelType', 'tripItineraries', 'tripItineraries.tripActivities', 'placesToVisits', 'tags', 'tripSubCategory', 'tripCategory'])
-            ->first();
-
-
-        if(count($package)>0)
-        {
-            $tripItinerary=$package->tripItineraries;
-            $placesToVisits=$package->placesToVisits;
-            return view('homepage.package_detailed',compact('tripItinerary','placesToVisits','package'));
-
-
-        }else
-        {
+        if (!empty($packages)) {
+            return view('homepage.booking', ['packages' => $packages]);
+        } else {
             return redirect('homepage');
         }
-
-
-
     }
 
-}
+    public function packageDetailed($id = null)
+    {
 
-public function processBooking(Request $request)
-{
+        if (!empty($id)) {
+            $package = Package::where('id', $id)
+                ->with(['travelType', 'tripItineraries', 'tripItineraries.tripActivities', 'placesToVisits', 'tags', 'tripSubCategory', 'tripCategory'])
+                ->first();
 
 
-    $randum_string="TXN_".substr(str_shuffle(str_repeat($x='123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(5/strlen($x)) )),1,5);
+            if (count($package) > 0) {
+                $tripItinerary = $package->tripItineraries;
+                $placesToVisits = $package->placesToVisits;
+                return view('homepage.package_detailed', compact('tripItinerary', 'placesToVisits', 'package'));
 
 
-        if(!empty($request->all()))
-        {
-            $booking= new Booking();
-            ['package_id', 'customer_name', 'contact_number', 'email_id', 'travel_date', 'adult', 'children','booking_reference'];
-
-            $booking->package_id=$request->package_id;
-            $booking->customer_name=$request->customer_name;
-            $booking->contact_number=$request->contact_number;
-            $booking->email_id=$request->email_id;
-            $booking->travel_date=$request->travel_date;
-            $booking->adult=$request->adult;
-            $booking->children=$request->children;
-            $booking->booking_reference=$randum_string;
-            if($booking->save())
-            {
-                $data=array('booking_id'=>$randum_string,'heading'=>'Thank you for booking !','message'=>" Your booking has been confirmed, please use the booking ID for the future reference");
-                return view('homepage.booking_confirm',compact('data'));
-            }else
-            {
-                $data=['booking_id'=>null,'heading'=>'Booking Failed ','message'=>" Your booking has beend cancelled please ook again"];
-                return view('homepage.booking_confirm',compact('data'));
+            } else {
+                return redirect('homepage');
             }
 
 
+        }
+
+    }
+
+    public function processBooking(Request $request)
+    {
+
+
+        $randum_string = "TXN_" . substr(str_shuffle(str_repeat($x = '123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil(5 / strlen($x)))), 1, 5);
+
+
+        if (!empty($request->all())) {
+            $booking = new Booking();
+            ['package_id', 'customer_name', 'contact_number', 'email_id', 'travel_date', 'adult', 'children', 'booking_reference'];
+
+            $booking->package_id = $request->package_id;
+            $booking->customer_name = $request->customer_name;
+            $booking->contact_number = $request->contact_number;
+            $booking->email_id = $request->email_id;
+            $booking->travel_date = $request->travel_date;
+            $booking->adult = $request->adult;
+            $booking->children = $request->children;
+            $booking->booking_reference = $randum_string;
+            if ($booking->save()) {
+                $data = array('booking_id' => $randum_string, 'heading' => 'Thank you for booking !', 'message' => " Your booking has been confirmed, please use the booking ID for the future reference");
+                return view('homepage.booking_confirm', compact('data'));
+            } else {
+                $data = ['booking_id' => null, 'heading' => 'Booking Failed ', 'message' => " Your booking has beend cancelled please ook again"];
+                return view('homepage.booking_confirm', compact('data'));
+            }
+
 
         }
         return redirect('homepage');
 
 
+    }
 
-}
+    public function bookingInfo($id = null)
+    {
+        if (!empty($id)) {
+
+            $get_booking_details=Booking::where('booking_reference',$id)->first();
+            if(!$get_booking_details->isEmpty)
+            {
+
+            }
+        }
+
+    }
 
 
-    public function packageDetails($id=null)
+    public function packageDetails($id = null)
     {
 
     }
